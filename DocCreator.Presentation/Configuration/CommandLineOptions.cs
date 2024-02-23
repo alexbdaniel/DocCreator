5908114030a1d31823dee5d0c1cc4af299b6f1ed
Alex Daniel <alexdaniel@outlook.com.au>
2024-02-23 21:42:58 +0800

    g

--- DocCreator.Presentation/Configuration/CommandLineOptions.cs
+++ DocCreator.Presentation/Configuration/CommandLineOptions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using CommandLine;
 
 namespace DocCreator.Presentation.Configuration;
-internal class CommandLineOptions
+public class CommandLineOptions
 {
 	[Option('p', "docargsfullpath", Required = true, HelpText = "Full path to the document arguments")]
 	public string DocArgsFullPath { get; set; }
--- DocCreator.Presentation/Program.cs
+++ DocCreator.Presentation/Program.cs
@@ -19,29 +19,34 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		var options = Parser.Default.ParseArguments<CommandLineOptions>(args);
+		var options = Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(options =>
+		{
+			
+
+			string? docArgsFullPath = options.DocArgsFullPath;
+
+			if (string.IsNullOrWhiteSpace(docArgsFullPath))
+			{
+				Console.WriteLine("Specify a valid path to the document arguments");
+				return;
+			}
+			else
+			{
+				docArgsFullPath = Cleaning.CleanPath(docArgsFullPath);
+			}
+
+			var result = new Creator().CreateDocsHandlerAsync(docArgsFullPath).Result;
+
+			var log = new LogSetup<DocCreationResponse>();
+
+			string logFullPath = log.CreateLogFile();
+			log.WriteToLogFile(result, logFullPath);
+
+			Console.WriteLine(Helper.CreateLogMessage(result, logFullPath));
+
+		});
 		
 
-		string? docArgsFullPath = options.Value.DocArgsFullPath;
-
-		if (string.IsNullOrWhiteSpace(docArgsFullPath))
-		{
-			Console.WriteLine("Specify a valid path to the document arguments");
-			return;
-		}
-		else
-		{
-			docArgsFullPath = Cleaning.CleanPath(docArgsFullPath);
-		}
-			
-		var result = new Creator().CreateDocsHandlerAsync(docArgsFullPath).Result;
-
-		var log = new LogSetup<DocCreationResponse>();
-
-		string logFullPath = log.CreateLogFile();
-		log.WriteToLogFile(result, logFullPath);
-
-		Console.WriteLine(Helper.CreateLogMessage(result, logFullPath));
 
 
 
